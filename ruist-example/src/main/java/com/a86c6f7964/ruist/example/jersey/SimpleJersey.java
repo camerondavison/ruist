ae74ad6606718dd43d46c9ae0de9dd7d189b3bc1
a86c6f7964 <cameron.davison@gmail.com>
2014-06-22 22:04:03 -0500

    for real decouple jersey from jetty now

--- ruist-example/src/main/java/com/a86c6f7964/ruist/example/jersey/SimpleJersey.java
+++ ruist-example/src/main/java/com/a86c6f7964/ruist/example/jersey/SimpleJersey.java
@@ -2,10 +2,14 @@ package com.a86c6f7964.ruist.example.jersey;
 
 import com.a86c6f7964.ruist.core.Ruist;
 import com.a86c6f7964.ruist.core.Ruister;
+import com.a86c6f7964.ruist.example.tool.SimpleTool;
+import com.a86c6f7964.ruist.example.tool.SimpleTool.SimpleService;
 import com.a86c6f7964.ruist.jersey.JerseyModule;
+import com.a86c6f7964.ruist.jetty.JettyServerModule;
 import com.a86c6f7964.ruist.varexport.VarExportModule;
 import com.a88c6f7964.ruist.status.StatusWebModule;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.util.concurrent.AbstractScheduledService;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.indeed.status.core.Dependency;
@@ -16,6 +20,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nonnull;
 import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -25,9 +30,14 @@ public class SimpleJersey {
 
     public static void main(String[] args) throws Exception {
         final Injector injector = Guice.createInjector(
-                new VarExportModule(), // order matters! jersey is /* so will pick up everything left
+                Ruister.services(SimpleService.class),
+
+                new VarExportModule(),
                 new StatusWebModule(ImmutableSet.<Class<? extends Dependency>>of(SimpleDep.class)),
-                new JerseyModule("com.a86c6f7964.ruist.example.jersey", new InetSocketAddress("localhost", 9900))
+
+                // order matters! jersey is /* so will pick up everything left
+                new JerseyModule("com.a86c6f7964.ruist.example.jersey"),
+                new JettyServerModule(new InetSocketAddress("localhost", 9900))
         );
 
         // start
@@ -43,6 +53,18 @@ public class SimpleJersey {
         @Override
         public void ping() throws Exception {
             log.info("PING simple dep");
+        }
+    }
+
+    public static class SimpleService extends AbstractScheduledService {
+        @Override
+        protected void runOneIteration() throws Exception {
+            log.info("Running!");
+        }
+
+        @Override
+        protected Scheduler scheduler() {
+            return Scheduler.newFixedRateSchedule(0, 1, TimeUnit.SECONDS);
         }
     }
 }
--- ruist-jersey/src/main/java/com/a86c6f7964/ruist/jersey/JerseyModule.java
+++ ruist-jersey/src/main/java/com/a86c6f7964/ruist/jersey/JerseyModule.java
@@ -1,6 +1,5 @@
 package com.a86c6f7964.ruist.jersey;
 
-import com.a86c6f7964.ruist.jetty.JettyServerModule;
 import com.google.inject.AbstractModule;
 import com.google.inject.servlet.ServletModule;
 import com.sun.jersey.api.core.PackagesResourceConfig;
@@ -8,25 +7,20 @@ import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 
 import javax.annotation.Nonnull;
-import java.net.InetSocketAddress;
 
 /**
  *
  */
 public final class JerseyModule extends AbstractModule {
     @Nonnull
-    private final InetSocketAddress address;
-    @Nonnull
     private final String packageName;
 
-    public JerseyModule(@Nonnull final String packageName, @Nonnull final InetSocketAddress address) {
-        this.address = address;
+    public JerseyModule(@Nonnull final String packageName) {
         this.packageName = packageName;
     }
 
     @Override
     protected void configure() {
-        install(new JettyServerModule(address));
         install(new ServletModule() {
             @Override
             protected void configureServlets() {
