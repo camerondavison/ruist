f37158baf5bdba05964e4e97bc1dcdaebef362b1
a86c6f7964 <cameron.davison@gmail.com>
2014-06-22 20:23:01 -0500

    make singleton

--- ruist-example/src/main/java/com/a86c6f7964/ruist/example/jersey/SimpleRest.java
+++ ruist-example/src/main/java/com/a86c6f7964/ruist/example/jersey/SimpleRest.java
@@ -1,11 +1,13 @@
 package com.a86c6f7964.ruist.example.jersey;
 
+import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 /**
  *
  */
+@Singleton
 @Path("/api")
 public class SimpleRest {
     @GET()
