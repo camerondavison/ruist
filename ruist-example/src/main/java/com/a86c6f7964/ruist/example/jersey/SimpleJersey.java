2c9c93575f65a0b31508585f8bb5a9d6bb6506f4
a86c6f7964 <cameron.davison@gmail.com>
2014-06-22 20:50:26 -0500

    adding var export to the jersey example

--- ruist-example/src/main/java/com/a86c6f7964/ruist/example/jersey/SimpleJersey.java
+++ ruist-example/src/main/java/com/a86c6f7964/ruist/example/jersey/SimpleJersey.java
@@ -2,6 +2,7 @@ package com.a86c6f7964.ruist.example.jersey;
 
 import com.a86c6f7964.ruist.core.Ruist;
 import com.a86c6f7964.ruist.jersey.JerseyModule;
+import com.a86c6f7964.ruist.varexport.VarExportModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
@@ -14,6 +15,7 @@ public class SimpleJersey {
 
     public static void main(String[] args) throws Exception {
         final Injector injector = Guice.createInjector(
+                new VarExportModule(), // order matters! jersey is /* so will pick up everything left
                 new JerseyModule("com.a86c6f7964.ruist.example.jersey", new InetSocketAddress("localhost", 9900))
         );
 
--- ruist-example/src/main/java/com/a86c6f7964/ruist/example/jersey/SimpleRest.java
+++ ruist-example/src/main/java/com/a86c6f7964/ruist/example/jersey/SimpleRest.java
@@ -1,5 +1,8 @@
 package com.a86c6f7964.ruist.example.jersey;
 
+import com.indeed.util.varexport.Export;
+import com.indeed.util.varexport.VarExporter;
+
 import javax.inject.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,9 +13,21 @@ import javax.ws.rs.Path;
 @Singleton
 @Path("/api")
 public class SimpleRest {
+    private volatile long i = 0;
+
+    public SimpleRest() {
+        VarExporter.forNamespace(getClass().getSimpleName()).includeInGlobal().export(this, "");
+    }
+
+    @Export(name = "i")
+    public long getI() {
+        return i;
+    }
+
     @GET()
     @Path("/ruok")
     public String ruok() {
+        i++;
         return "imok";
     }
 }
